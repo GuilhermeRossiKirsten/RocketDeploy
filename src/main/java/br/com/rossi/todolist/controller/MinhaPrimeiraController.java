acf9e2b8629fc373b47f331692cd7e9e72245edd
Guilherme <guirossik10@gmail.com>
2023-10-15 13:07:19 -0300

    Deploy

---
+++ src/main/java/br/com/rossi/todolist/controller/MinhaPrimeiraController.java
@@ -0,0 +1,24 @@
+package br.com.rossi.todolist.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
+@RequestMapping("/primeiraRota")
+public class MinhaPrimeiraController {
+    /** *
+     * GET - Buscar uma informação
+     * POST - Adicionar um dado/informação
+     * PUT - Alterar um dado/info 
+     * DELETE - Remover um dado
+     * PATCH - Alterar somente uma parte da info/dado
+    */
+    
+    @GetMapping("/")
+    public String primeiraMensagem() {
+        return "Funcionou";
+    }
+}
